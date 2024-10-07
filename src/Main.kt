e3f2971c48a3e0c7b51e7be689c81617919b5d34
adievskiy <dievskiy@mail.ru>
2024-10-07 17:51:57 +0400

    Домашнее задание по теме "Filter"

--- src/Main.kt
+++ src/Main.kt
@@ -2,4 +2,5 @@ fun main() {
     val test = mapOf(1 to "Анна", 2 to "Борис", 3 to "Георгий")
     println(test.filter { it.key < 3 })
     println(test.filter { it.value.endsWith("а") })
+    println(test.filter { it.value.startsWith("Г") })
 }
